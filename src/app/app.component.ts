e67538a842356fbc84fcf83d17694abba2f3835e
zenisha-savaliya <zenisha.savaliya@bacancy.com>
2024-07-23 15:54:34 +0530

    auto complete example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'learning-prime-ng';
+  public suggestions: string[] = [];
+  public countries: string[] = [
+    'Australia',
+    'Brazil',
+    'Canada',
+    'Denmark',
+    'Egypt',
+    'France',
+    'Germany',
+    'India',
+    'Japan',
+    'Kenya',
+  ];
+
+  public search(event: any): void {
+    this.suggestions = this.countries.filter((country) =>
+      country.toLowerCase().includes(event.query.toLowerCase())
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AutoCompleteModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
