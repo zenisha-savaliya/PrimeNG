eab3c19326f20acd89b93ab82df7aa91c046cfd2
zenisha-savaliya <zenisha.savaliya@bacancy.com>
2025-07-02 12:21:43 +0530

    primeng last changes

--- src/app/store/todo.effects.ts
+++ src/app/store/todo.effects.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { ToDoService } from '../services/to-do.service';
-import { catchError, filter, map, of, switchMap, take } from 'rxjs';
-import { loadTodos, loadTodosFailure, loadTodosSuccess } from './todo.actions';
+import { catchError, map, of, switchMap, take } from 'rxjs';
 import { IToDo } from '../interfaces/todo-interface';
+import { ToDoService } from '../services/to-do.service';
+import { loadTodos, loadTodosFailure, loadTodosSuccess } from './todo.actions';
 @Injectable()
 export class TodosEffects {
   constructor(private actions$: Actions, private todoService: ToDoService) {}
@@ -13,8 +13,6 @@ export class TodosEffects {
       ofType(loadTodos),
       switchMap(() =>
         this.todoService.getTodoList().pipe(
-          take(1),
-          filter((todos) => !!todos.length),
           map((todos: IToDo[]) => {
             return loadTodosSuccess({ todos });
           }),
